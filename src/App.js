424fb9e812b782951c7338d81d94dea282319706
whdgkr1127 <79003007+whdgkr1127@users.noreply.github.com>
2021-05-09 23:04:02 +0900

    movie_app_2021Update5

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import React from 'react';
 class App extends React.Component{
   state = {
     isLoading:true,
-    movie:true
+    movie:true//state는 필수가 아니다.
   }
 
   componentDidMount(){
     setTimeout(() => { //delay function이다.
       this.setState({ isLoading:false })
-    }, 6000)
+    }, 6000)//밀리터리 단위이기 때문에 1초는 1000으로 표현한다.
   }
 
   render(){
-   const { isLoading } = this.state;
+   const { isLoading } = this.state;//state 안에있는 isLoading을 불러오는 코드이다.(ES6 참고)
     return  <div>{isLoading ? "Loading...":"We are ready"}</div>; 
      }
 }
